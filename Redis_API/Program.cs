d0b73d35e555ee956c064e587e51a9f1a1088ea0
rahma-mohmed <engrahma1422003@gmail.com>
2025-02-07 22:57:00 +0200

    register memory cache service builder.Services.AddMemoryCache();

---
+++ Redis_API/Program.cs
@@ -0,0 +1,39 @@
+
+namespace Redis_API
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddControllers();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			#region Memory Cache
+
+			// Add memory cache services
+			builder.Services.AddMemoryCache();
+
+			#endregion
+
+			var app = builder.Build();
+
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
